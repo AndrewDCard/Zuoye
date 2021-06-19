2ce83a3377588744fbedfc2ead56f31361d93f08
AndrewDCard <85871570+AndrewDCard@users.noreply.github.com>
2021-06-19 23:20:26 +0800

    Delete WEB作业 directory

--- WEB作业/js/lefttorig.js
+++
@@ -1,9 +0,0 @@
-function animate(obj, target) {
-    clearInterval(obj.timer);
-    obj.timer = setInterval(function() {
-        if (obj.offsetLeft >= target)
-            clearInterval(obj.timer);
-        var step = (target - obj.offsetLeft) / 10;
-        obj.style.left = obj.offsetLeft + step + "px";
-    }, 30)
-}
